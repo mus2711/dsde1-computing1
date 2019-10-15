6f84b7eeda8dbb8cb45b22f2388d2785aa58c6db
mus2711 <ma819@ic.ac.uk>
2019-10-15 16:00:16 +0100

    with elif

--- Week-3/guess.py
+++ Week-3/guess.py
@@ -2,15 +2,18 @@ import random as r
 number = (r.randint(1,10))
 print('Choose a number between 1 to 10')
 i = 0
+
 while i < 3:
-    x = input()
+    x = input('Your guess: ' )
     guess = int(x)
-    if guess == number:
+    if guess > 10 or guess < 1:
+        print('Please choose a number only between 1 and 10.')
+    elif guess == number:
         print('You guessed correct!')
         break
     else:
         i += 1
-        wrong = 'You guessed the wrong number, try again. Remaining guesses: {}'
+        wrong = 'You guessed the wrong number. Remaining guesses: {}'
         print(wrong.format(3-i))
         
 if i == 3:
