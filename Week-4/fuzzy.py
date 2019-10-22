b14b9e3206770825b29c473e8340540d67fbf966
mus2711 <ma819@ic.ac.uk>
2019-10-22 21:30:10 +0100

    22/10 push

--- Week-4/fuzzy.py
+++ Week-4/fuzzy.py
@@ -6,27 +6,25 @@ Lint this file using PyLint.
 
 # This function does some maths on three numbers.
 def maths(input_a, input_b, input_c):
+    ''' This function will multiply the first input by 3,
+    then subtract the second and add the third inputs respectively.
+    '''
     result = input_a * 3 - input_b + input_c
     return result
 
 # This function returns True or False.
 def choices(question):
-    if(question == True):
-        return True
-    else:
-        return False
-
+    '''Makes sure a question is asked, will return True if so.'''
+    return bool(question)
 
 def main():
-    this_variable = 'useless'
-    
+    '''Will execute both the previous functions.'''
     # first function takes three numbers
-    answer = maths(3, 9 , 2.3)
+    answer = maths(3, 9, 2.3)
     print(answer)
-
     # second function takes a True or False
-    newAnswer = choices(True)
-    print(newAnswer)
+    new_answer = choices(True)
+    print(new_answer)
 
 if __name__ == '__main__':
     main()
--- Week-4/structures.py
+++ Week-4/structures.py
@@ -9,26 +9,32 @@ Simple functions performing operations on basic Python data structures.
 # write a function that returns a list containig the first and the last element
 # of "the_list". 
 def first_and_last(the_list):
-    return the_list[-1:0]
-
-
+    '''Will return first and last element of input.'''
+    new_list = []
+    new_list.append(the_list[0])
+    new_list.append(the_list[-1])
+    return new_list
+    
 # write a function that returns part of "the_list" between indices given by the
 # second and third parameter, respectively. The returned part should be in
 # reverse order than in the original "the_list". 
 # If "end" is greater then "beginning" or any og the indices is out of the
 # list, raise a "ValueError" exception. 
-def part_reverse(the_list, beginning, end):
+def parameter_list(the_list, beginning, end):
     if beginning < end:
-        return the_list[beginning:end:-1] 
+        new_list = the_list[beginning:end]
+        new_list.reverse()
+        return new_list
     else:
         return ValueError
+
 # write a function that at the "index" of "the_list" inserts three times the
 # same value. For example if the_list = [0,1,2,3,4] and index = 3 the function
 # will return [0,1,2,3,3,3,4]. 
 def repeat_at_index(the_list, index):
-    new_list = the_list.insert(index, the_list[index]*2)
-    return new_list
-
+    for x in range(2):
+        the_list.insert(index, the_list[index])
+    return the_list
 
 # Strings
 
@@ -43,17 +49,16 @@ def palindrome_word(word):
 # read the same forward and backward. Ignore all spaces and other characters
 # like fullstops, commas, etc. Also do not consider whether the letter is
 # capital or not. 
-# def palindrome_sentence(sentence):
-#     # import re
-#     # sentence = re.sub(r'[^\w\s]','')
-#     # new_sentence = sentence.lower()
-#     # new_string = ''.join(new_sentence.split())
-#     # print(new_string)
-#     if new_string[::1] == new_string[::-1]:
-#         return True
-#     else:
-#         return False
-# print(palindrome_sentence('Mr. Owl ate my metal worm'))
+def palindrome_sentence(sentence):
+    import string
+    valid = set(string.ascii_letters)
+    sentence = sentence.lower()
+    s = ''.join([x for x in sentence if x in valid])
+    if s == s[::-1]:
+        return 'Sentence is a palindrome.'
+    else:
+        return "Sentence is not a palindrome"
+
 # write a function that concatenates two sentences. First the function checks
 # whether the sentence meets the following criteria: it starts with a capital
 # letter and it ends with a full stop, question mark, or an exclamation point.
@@ -62,9 +67,9 @@ def palindrome_word(word):
 # or at the end and the must be exactly one space after the end of the first
 # sentence. 
 def concatenate_sentences(sentenece1, sentence2):
-    ns1 = sentenece1.lstrip()
-    ns2 = sentence2.lstrip()
-    ns3 = ns1 + ' ' + ns2
+    sentenece1.lstrip()
+    sentence2.lstrip()
+    ns3 = sentenece1 + ' ' + sentence2
     new_list = ns3.split()
     if new_list[0][0].isupper():
         if new_list[-1][-1] in ['.','!','?']:
@@ -75,9 +80,6 @@ def concatenate_sentences(sentenece1, sentence2):
 
     else:
         return 'Please make sure that your clauses are gramatically correct.'
-# print(concatenate_sentences('    Hello', 'my friend.    '))
-
-
 
 # Dictionaries
 
