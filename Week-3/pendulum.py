f7bd00f728568bb64e8be807281ad433bf2457a1
mus2711 <ma819@ic.ac.uk>
2019-10-24 14:50:03 +0100

    updated

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,14 +1,12 @@
 def period(length , gravity):
     import math as m
-    if isinstance(length, (float, int)):
-        if length <= 0:
-            return 'Input positive numbers only.'
-        if gravity <= 0:
+    if isinstance(length, (float, int)) and isinstance(gravity, (float, int)):
+        if length <= 0 or gravity <=0:
             return 'Input positive numbers only.'
         else:
             return 2*m.pi*((length/gravity)**0.5)
     else: 
-        return 'Please input only numbers.'
+        raise(TypeError)
 
-# print(period(7.5,-9.8))
+print(period(7.5,'helo'))
             
